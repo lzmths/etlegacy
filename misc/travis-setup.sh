c4599ebf730f4c353f2752bd9401b645d1a300ca
Remy Marquis <remy.marquis@gmail.com>
2015-10-09 12:09:27 +0200

    misc: travis fix

--- misc/travis-setup.sh
+++ misc/travis-setup.sh
@@ -8,7 +8,7 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
   sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 40976EAF437D05B5
   sudo apt-get update -qq
   sudo apt-get install -q -t precise pkg-config nasm libgl1-mesa-dev libasound2-dev pulseaudio m4
-  sudo apt-get install -q -t trusty automake
+  sudo apt-get install -q -t wily automake
 elif [[ "$OSTYPE" == "darwin"* ]]; then
 	# we need the gnu-sed from brew for r2
 	echo "Travis running apple build"
